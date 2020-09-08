7bc3eb419a308a05a50a376152b9d902460472c8
Guilherme Ferreira <guihensanfer@gmail.com>
2020-09-08 15:03:20 -0300

    Página Sobre ok, primeira versão

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace Bom_Dev.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Sobre()
         {
             return View();
         }
