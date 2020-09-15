522255d645cd3146a8f54a8b41336953f674c54c
Guilherme Ferreira <guihensanfer@gmail.com>
2020-09-15 00:10:34 -0300

    Listando aplcação na tela home

--- Models/Projetos.cs
+++ Models/Projetos.cs
@@ -8,16 +8,42 @@ namespace Bom_Dev.Models
         public enum Setores
         {
             [Display(Name = "Finanças & Investimentos")]
-            Financas_Investimentos = 1
+            Financas_Investimentos = 1,
+
+            [Display(Name = "Vendas na internet")]
+            VendasPelaInternet = 2
         }    
+
+        public struct Cores
+        {
+            public System.Drawing.Color Texto{get;set;}
+            public System.Drawing.Color Fundo{get;set;}
+        }
         
         public class Projeto
         {
+            public Projeto()
+            {
+                Ativo = true;
+                CorIdentidade = new Cores{
+                    Texto = System.Drawing.Color.Black,
+                    Fundo =  System.Drawing.Color.White
+                };
+            }
+
             public string Nome{get;set;}
             public string DescricaoBreve{get;set;}
             public string DescricaoDetalhada{get;set;}
+            public Setores Setor{get;set;}
             public int VersaoCompilacao{get;set;}
             public string LinkAcesso{get;set;}
+            public bool Ativo{get;set;}
+            public Cores CorIdentidade{get;set;}
+            public bool EmDesenvolvimentoInicial{
+                get{
+                    return VersaoCompilacao <= 0;
+                }
+            }
         }
 
         public static List<Projeto> InstanciarProjetos() => new List<Projeto>(){
@@ -25,8 +51,14 @@ namespace Bom_Dev.Models
                 Nome = "Controle de despesas pessoais",
                 DescricaoBreve = "Organização de pendências financeiras  pessoais.",
                 DescricaoDetalhada = "Organize suas pendências financeiras de forma fácil e simples. Você possui despesas trimestrais, semestrais ou anuais? Garanta o recurso financeiro para compri-lás e ainda ganhe juros da poupança com isso.",
-                LinkAcesso = "#",
-                VersaoCompilacao = 1
+                LinkAcesso = "https://www.google.com.br/",
+                VersaoCompilacao = 0,
+                Setor = Setores.Financas_Investimentos,
+                Ativo = true,
+                CorIdentidade = new Cores{
+                    Texto = System.Drawing.Color.FromArgb(49, 212, 117), // Verde
+                    Fundo = System.Drawing.Color.FromArgb(50, 50, 50) // Quase preto
+                }
             }
         };
     }
