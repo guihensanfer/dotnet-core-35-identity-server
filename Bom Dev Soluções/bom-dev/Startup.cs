99051cbd936d4d5e5ba6e8ea1f7334473c84a37d
Guilherme Ferreira <guihensanfer@gmail.com>
2020-12-09 11:06:15 -0300

    Voltei o client para configuração inicial somente com Identity e tabelas

--- Bom Dev Soluções/bom-dev/Startup.cs
+++ Bom Dev Soluções/bom-dev/Startup.cs
@@ -1,25 +1,12 @@
-using System.IdentityModel.Tokens.Jwt;
-using System.Net;
-using System.Net.Http;
-using System.Net.Security;
-using System.Security.Authentication;
-using System.Security.Claims;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
 using Bom_Dev.Data;
 using Bom_Dev.Models;
-using IdentityModel;
-using IdentityServer4.AccessTokenValidation;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Logging;
 
 namespace Bom_Dev
 {
@@ -37,78 +24,38 @@ namespace Bom_Dev
         {     
             services.AddControllersWithViews();
             services.AddRazorPages();
-            services.AddTransient<IEmailSender, EmailConfiguracao>();
-
-            // Identity Server
-            services.AddIdentityServer(o => {
-                o.Events.RaiseErrorEvents = true;
-                o.Events.RaiseInformationEvents = true;
-                o.Events.RaiseFailureEvents = true;
-                o.Events.RaiseSuccessEvents = true;                                
-            })            
-            .AddDeveloperSigningCredential()
-            .AddInMemoryApiScopes(Config.GetApiScopes())            
-            .AddInMemoryClients(Config.GetClients())
-            .AddTestUsers(Config.GetUsers())
-            .AddJwtBearerClientAuthentication()           
-            .AddDeveloperSigningCredential() 
-            .AddInMemoryApiResources(Config.GetApiResources());         
-           
-            // .AddAspNetIdentity<BomDevUser>();
-
-            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
-            .AddIdentityServerAuthentication(options =>
-            {
-                options.Authority = "https://localhost:5001";                    
-                options.RequireHttpsMetadata = false;
-                options.ApiName = "api1";                                                                                                                                                            
-            });                                           
+            services.AddTransient<IEmailSender, EmailConfiguracao>();                                                                         
 
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
+            #region Identity
             services.AddDefaultIdentity<BomDevUser>(options => {
                 options.SignIn.RequireConfirmedAccount = true;
-                
+
                 // Senha
                 options.Password.RequiredLength = 8;
                 options.Password.RequireLowercase = false;
                 options.Password.RequireUppercase = false;
-                options.Password.RequireNonAlphanumeric = false;      
+                options.Password.RequireNonAlphanumeric = false;
 
                 // Máximo tentativas login
-                options.Lockout.MaxFailedAccessAttempts = 3;              
-            })            
+                options.Lockout.MaxFailedAccessAttempts = 3;
+            })
             .AddEntityFrameworkStores<ApplicationDbContext>();
-
             // Login com Google
             services.AddAuthentication().AddGoogle(g =>
             {
                 g.ClientSecret = Configuration.GetValue<string>("GoogleLogin:ClientSecret");
                 g.ClientId = Configuration.GetValue<string>("GoogleLogin:ClientId");
-            });                                                                           
+            });
+            #endregion
         }
-
-        public static bool ValidateServerCertificate(object sender,X509Certificate certificate,X509Chain chain,SslPolicyErrors sslPolicyErrors)
-        {
-            return true;
-        }
-
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {  
-            IdentityModelEventSource.ShowPII = true;                      
-            ServicePointManager.Expect100Continue = true;
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
-            | SecurityProtocolType.Tls11
-            | SecurityProtocolType.Tls12;
-            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, errors) =>
-            {
-                // local dev, just approve all certs
-                if (env.IsDevelopment()) return true;
-                return errors == SslPolicyErrors.None ;
-            };
-
+        {             
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -124,10 +71,8 @@ namespace Bom_Dev
             app.UseStaticFiles();
 
             app.UseRouting();    
-            
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-            app.UseAuthentication();
-            app.UseIdentityServer();      
+                        
+            app.UseAuthentication();             
             app.UseAuthorization();   
 
             app.UseEndpoints(endpoints =>
