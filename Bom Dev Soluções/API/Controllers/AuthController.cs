f86a24096ffc461d454ef9ea12ba7529e8a62eb9
Guilherme Ferreira <guihensanfer@gmail.com>
2020-12-10 08:50:53 -0300

    Integrando api com identityServer

---
+++ Bom Dev Soluções/API/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(Roles = "Admin")]
+    public class AuthController : ControllerBase
+    {        
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Acesso liberado!" };
+        }
+    }
+}
